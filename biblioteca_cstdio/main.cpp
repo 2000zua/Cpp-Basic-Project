3a4f88ffb4e7ba53d78392a4820ad281268eb2d6
Angelo Abreu Zua <Gildozua1@hotmail.com>
2023-10-26 13:04:38 +0300

    pag 79
    pag 79

---
+++ biblioteca_cstdio/main.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /**  putchat()
+     * Imprimi um unico carracter na tela
+     * */
+    char c = '$';
+    putchar(c);
+
+    /**
+     * puts()
+     * Imprime uma cadeia de caracter na no console
+    
+    */
+   puts("Ola mundo complicado!");
+
+   /**
+    * getchar()
+    * Pegar um unico caracter digitado no console
+   */
+  char unico = getchar();
+  cout <<unico<<endl;
+  
+
+
+
+
+    return 0;
+}
